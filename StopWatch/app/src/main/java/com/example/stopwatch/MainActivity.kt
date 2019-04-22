e9b3d7291acf23c462cf6d52b692f88328caf5de
lenox24 <yk03171@naver.com>
2019-04-22 23:48:14 +0900

    set textview

--- StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.stopwatch
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView.setOnClickListener {
+            val str: Int = editText.text.toString().toInt()
+
+            Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
+        }
     }
 }
