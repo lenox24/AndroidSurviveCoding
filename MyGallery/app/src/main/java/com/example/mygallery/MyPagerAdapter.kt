82388f5ec5871cd8263d641689f3ffd50af7a719
lenox24 <yk03171@naver.com>
2019-04-26 19:25:04 +0900

    project start & git init

---
+++ MyGallery/app/src/main/java/com/example/mygallery/MyPagerAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.mygallery
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+
+class MyPagerAdapter(fm: FragmentManager?) : FragmentStatePagerAdapter(fm) {
+
+    private val items = ArrayList<Fragment>()
+
+    override fun getItem(position: Int): Fragment {
+        return items[position]
+    }
+
+    override fun getCount(): Int {
+        return items.size
+    }
+
+    fun updateFragments(item: List<Fragment>) {
+        this.items.addAll(items)
+    }
+
+}
