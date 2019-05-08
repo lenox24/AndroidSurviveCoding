0404c3e49588a6d4a4760a7d797ecbc52b9f5d41
lenox24 <yk03171@naver.com>
2019-05-08 20:01:18 +0900

    project init && error

--- Xylophone/app/src/main/java/com/example/xylophone/MainActivity.kt
+++ Xylophone/app/src/main/java/com/example/xylophone/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.xylophone
 
+import android.content.pm.ActivityInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
